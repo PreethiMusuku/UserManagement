75ae95efae241f349baa323e7e3a1cb6f5c08e13
Preethi Musuku <preethimusuku@gmail.com>
2020-07-17 10:24:11 +0530

    second commit

--- UserManagementSystem/src/test/java/com/cg/usermanagement/UserManagementSystemApplicationTests.java
+++ UserManagementSystem/src/test/java/com/cg/usermanagement/UserManagementSystemApplicationTests.java
@@ -3,20 +3,13 @@ package com.cg.usermanagement;
 import java.net.URI;
 import java.net.URISyntaxException;
 
-
-import java.net.URI;
-import java.net.URISyntaxException;
-
 import org.junit.Test;
-//import org.junit.Test;
+
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
+
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
-
-import com.cg.usermanagement.entity.UserDto;
 
 import junit.framework.Assert;
 
@@ -25,7 +18,7 @@ public class UserManagementSystemApplicationTests {
 	@SuppressWarnings("deprecation")
 	
 		@Test
-		public void testDeleteDiagnosticCentreListSuccess() throws URISyntaxException {
+		public void testUserManagementDeleteSuccess() throws URISyntaxException {
 			RestTemplate restTemplate = new RestTemplate();
 			final String baseUrl = "http://localhost:7611/UserManagement/deleteuser/325";// Enter present id
 			URI uri = new URI(baseUrl);
